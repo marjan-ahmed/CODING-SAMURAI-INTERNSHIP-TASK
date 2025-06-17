63276c445f88a4549fdd7789f8e47d9d558d8bfb
Marjan Ahmed <marjan.ahmed08@yahoo.com>
2025-06-17 14:48:11 +0500

    Update main.py

--- 01_beginner_level/01_simple_calculator/main.py
+++ 01_beginner_level/01_simple_calculator/main.py
@@ -2,7 +2,7 @@ print("Welcome to Simple Calculator".center(50))
 
 num1 = int(input("Enter a number: "))
 num2 = int(input("Enter a number: "))
-opr = input("Enter an operation to perform (+, -, x, /): ")
+opr = input("Enter an operation to perform (+, -, *, /): ")
 
 match opr:
     case "+":
@@ -10,6 +10,6 @@ match opr:
     case "-":
         print(f"{num1} - {num2} = {num1 - num2}")
     case "x":
-        print(f"{num1} * {num2} = {num1 * num2}")
+        print(f"{num1} x {num2} = {num1 * num2}")
     case "/":
         print(f"{num1} / {num2} = {num1 / num2}")
